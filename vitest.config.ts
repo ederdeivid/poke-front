1cc6f6a85628c05d171e1853120dd4751b0d0b26
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-12 19:21:38 -0300

    fix: add back vitest config moved before to vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
   },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  }
 });
