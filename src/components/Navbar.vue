6412ecef3e1b51d416ae21e0aff13097e3085350
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-13 14:12:13 -0300

    feat: add logo to navbar and default page finished

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,16 +4,16 @@ import Logo from './Logo.vue';
 </script>
 
 <template>
-  <nav class="styck top-0 bg-white border-b border-slate-100">
+  <nav class="styck top-0 bg-white border-b-2">
     <div class="px-4">
       <div class="flex items-center justify-between h-16">
 
         <div class="flex">
-          <!-- <span>PokeApi</span> -->
           <Logo />
         </div>
 
-        <div class="flex"></div>
+        <div class="flex">
+        </div>
       </div>
     </div>
   </nav>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,14 +3,13 @@ import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-  <div class="flex h-screen bg-gray-500 overflow-hidden">
+  <div class="flex h-screen bg-[#f1f1f1] overflow-hidden">
 
     <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
       <Navbar />
 
       <main class="grow">
         <div class="p-4 w-full mx-auto">
-          content
           <slot />
         </div>
       </main>
