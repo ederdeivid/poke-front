5fe956527c9fc6a3d80b937d9ada54b9a5084d2b
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-13 13:25:26 -0300

    feat: initial navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-  <div></div>
+  <nav class="styck top-0 bg-white border-b border-slate-100">
+    <div class="px-4">
+      <div class="flex items-center justify-between h-16">
+
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style lang="css" scoped>
