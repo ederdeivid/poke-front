f8678b1bb88ae609453b2c33b5bd9d7b77666388
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-13 17:32:12 -0300

    chore: change vitest config to work with vue files

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
   },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
