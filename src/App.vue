7e95a83fc21786e4aa33181859f988e534b28bd5
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-13 13:20:34 -0300

    feat: add default page, create an empty navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import Default from '@/layouts/default.vue'
 </script>
 
 <template>
-  <div class="">
+  <Default>
     <router-view />
-  </div>
+  </Default>
 </template>
 
 <style scoped>
