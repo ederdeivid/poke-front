ee5dea9951914851c6286ee3ac31711161eeb2b7
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-13 17:32:40 -0300

    refactor: change how services work

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import './style.css';
 import App from './App.vue';
 import AxiosAdapter from './adapters/AxiosAdapter';
 import QueryStringAdpter from './adapters/QueryStringAdapter';
-import BaseService from './services/BaseService';
 import router from './routes';
+import Services from './services/Services';
 
 const httpClient: AxiosAdapter = new AxiosAdapter();
 const baseURL: string = 'https://pokeapi.co/api/v2/';
 const queryString: QueryStringAdpter = new QueryStringAdpter();
-const services: BaseService = new BaseService(httpClient, baseURL, queryString);
+const services: Services = new Services(httpClient, baseURL, queryString);
 
 const app = createApp(App);
 app.provide("services", services);
--- src/services/PokeAPIServices.ts
+++ src/services/PokeAPIServices.ts
@@ -1,7 +1,9 @@
 import { ISearchByLimit } from '@/types/PokemonApiRequestTypes';
-import BaseService from './BaseService';
+import HttpClient from '@/contracts/HttpClient';
+import QueryString from '@/contracts/QueryString';
 
-export default class PokeAPIServices extends BaseService {
+export default class PokeAPIServices {
+  constructor (private readonly httpClient: HttpClient, private readonly baseURL: string, private readonly queryString: QueryString) {}
   public async searchByLimit (searchParams: ISearchByLimit): Promise<any> {
     const pokemons = await this.httpClient.get(`${this.baseURL}/pokemon/?${this.queryString.stringify(searchParams)}`);
     return pokemons;
