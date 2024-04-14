ff14fbaef7918bc5875d05cdf5b85766c7204130
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 14:51:21 -0300

    feat: add partial badge component

---
+++ src/components/Badge.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const lalala = {
+  1: {
+    name: 'NORMAL',
+    style: 'bg-slate-100'
+  },
+  3: {
+    name: 'FLYING',
+    style: ''
+  },
+  4: {
+    name: 'POISON',
+    style: ''
+  },
+  5: {
+    name: 'GROUND',
+    style: ''
+  },
+  10: {
+    name: 'FIRE',
+    style: ''
+  },
+  11: {
+    name: 'WATER',
+    style: ''
+  },
+  12: {
+    name: 'GRASS',
+    style: ''
+  },
+  13: {
+    name: 'ELECTRIC',
+    style: ''
+  },
+  18: {
+    name: 'FAIRY',
+    style: ''
+  },
+  default: {
+    name: 'DEFAULT',
+    style: ''
+  },
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="css" scoped></style>
