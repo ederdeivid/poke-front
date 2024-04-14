9d6dcbb40828cfe394170f49346d0bacd5ad22e2
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 14:50:55 -0300

    fix: change input from text to search

--- src/views/Pokemons/PokemonSearch.vue
+++ src/views/Pokemons/PokemonSearch.vue
@@ -14,9 +14,9 @@ onMounted(() => searchPokemons())
 
 <template>
   <div class="flex flex-col lg:flex-row">
-    <div class="w-full mb-3">
+    <div class="w-full mb-12">
       <div class="flex flex-col items-center gap-2 mb-3">
-        <input type="text" class="w-1/2 form-input" placeholder="Digite o nome ou id do pokemon desejado">
+        <input type="search" class="w-full md:w-5/6 lg:w-5/6 form-input" placeholder="Pesquise por nome ou id do pokemon">
       </div>
     </div>
   </div>
