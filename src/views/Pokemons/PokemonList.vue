550ffc91930093b8a901c843d7e9cd09a8d3a066
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 14:58:14 -0300

    feat: add partial pokemons list

--- src/views/Pokemons/PokemonList.vue
+++ src/views/Pokemons/PokemonList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Card from '@/components/Card.vue';
 import PokemonSearch from './PokemonSearch.vue';
 
 </script>
@@ -7,7 +8,10 @@ import PokemonSearch from './PokemonSearch.vue';
   <div>
     <PokemonSearch />
 
-    <div class="w-full bg-white shadow-lg rounded-sm border border-slate-200 relative mr-2">
+    <div class="flex w-full justify-center">
+      <div class="w-full md:w-fill lg:w-1/2 items-center bg-white shadow-lg rounded-sm border border-slate-200 relative mr-2">
+        <Card />
+      </div>
     </div>
   </div>
 </template>
