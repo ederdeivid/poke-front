1231d885ef0a8b8d4cc53578c3148c194918b0bd
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 15:14:35 -0300

    refactor: remove hardcoded card component from PokemonList

--- src/views/Pokemons/PokemonList.vue
+++ src/views/Pokemons/PokemonList.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import Card from '@/components/Card.vue';
 import PokemonSearch from './PokemonSearch.vue';
 
 </script>
@@ -10,7 +9,6 @@ import PokemonSearch from './PokemonSearch.vue';
 
     <div class="flex w-full justify-center">
       <div class="w-full md:w-fill lg:w-1/2 items-center bg-white shadow-lg rounded-sm border border-slate-200 relative mr-2">
-        <Card />
       </div>
     </div>
   </div>
