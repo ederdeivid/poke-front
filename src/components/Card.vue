32d8b459a5143371fa2946b9f77c6e368f9c612e
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 15:13:50 -0300

    refactor: remove hardcoded card component data

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-// const props = defineProps<{ img: string, name: string, id: number | string, types: string[] }>()
+const props = defineProps<{ name: string, id: number | string }>()
 </script>
 
 <template>
   <div class="flex flex-col justify-between w-60 h-72 p-3 border border-cyan-500">
     <div class="flex flex-col">
       <div class="flex justify-center mb-3">
-        <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/4.svg" loading="lazy" decoding="async" fetchpriority="high" width="70" />
+        <img id="pokemon-image" :src="`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${props.id}.svg`" loading="lazy" decoding="async" fetchpriority="high" width="70" />
       </div>
 
-      <div class="font-semibold text-xl text-center mb-4">Pokemon name</div>
+      <div class="font-semibold text-xl text-center mb-4">
+        <span id="pokemon-name">{{ props.name }}</span>
+      </div>
 
-      <div class="font-semibold text-center">Código</div>
+      <div class="font-semibold text-center">
+        <span id="pokemon-id">Cod: {{ props.id }}</span>
+      </div>
     </div>
 
     <div class="flex flex-row justify-center">
