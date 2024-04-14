e6731f68a77a5e57e353e6103ab1bd04a5d6e73d
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-14 18:08:18 -0300

    refactor: add urlsearchparams as querystring adapter

--- src/adapters/QueryStringAdapter.ts
+++ src/adapters/QueryStringAdapter.ts
@@ -1,8 +1,7 @@
 import QueryString from '@/contracts/QueryString';
-import queryString from 'query-string';
 
 export default class QueryStringAdpter implements QueryString {
   stringify (params: Record<string, any>): string {
-    return queryString.stringify(params);
+    return new URLSearchParams(params).toString()
   }
 }
