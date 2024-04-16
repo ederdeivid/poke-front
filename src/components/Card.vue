62e4a63843e511b434350ff780d6a56e990f8f62
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-16 17:03:04 -0300

    refactor: renive deadcode and comments from card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-const props = defineProps<{ name: string, id: number | string }>()
+const props = defineProps<{ name: string, id: number | string; width?: string; height?: string; }>();
 </script>
 
 <template>
-  <div class="flex flex-col justify-between w-60 h-72 p-3 border border-cyan-500">
+  <div class="flex flex-col justify-between p-3 bg-white shadow-lg rounded-md cursor-pointer border hover:border-black/20">
     <div class="flex flex-col">
       <div class="flex justify-center mb-3">
-        <img id="pokemon-image" :src="`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${props.id}.svg`" loading="lazy" decoding="async" fetchpriority="high" width="70" />
+        <div class="w-[96px] h-[96px]">
+          <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`" id="pokemon-image" loading="lazy" decoding="async"
+            fetchpriority="high" width="96" />
+        </div>
       </div>
 
       <div class="font-semibold text-xl text-center mb-4">
@@ -18,7 +21,7 @@ const props = defineProps<{ name: string, id: number | string }>()
       </div>
     </div>
 
-    <div class="flex flex-row justify-center">
+    <div class="mb-2">
       <slot name="card-footer-content" />
     </div>
   </div>
