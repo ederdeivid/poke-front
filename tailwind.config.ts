841a9430cc343c5ba9394b33e01a56e0afd92801
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-16 16:59:04 -0300

    chore: add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        water: '#00A3FF',
+        grass: '#08FEC3',
+        poison: '#AF08FE',
+        fire: '#FE0808',
+        eletric: '#FFB800',
+        ground: '#85826E',
+        fairy: '#FBA1EC',
+        normal: '#C4C4C4',
+        flying: '#5317FC',
+        default: '#0E0E0E'
+      }
+    },
   },
   plugins: [],
 }
