2ef25ff6b7bbe2596ca7c193aef7a20b205d65e8
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-16 21:16:49 -0300

    refactor: remove dead code and useless variables

--- src/usecases/SearchPokemonByName.usecase.ts
+++ src/usecases/SearchPokemonByName.usecase.ts
@@ -4,14 +4,12 @@ import Services from '@/services/Services';
 import { PokemonDetailsDAO } from '@/types/PokemonApiRequestTypes';
 
 export default class SearchPokemonByNameUseCase {
-  private pokemonDetails!: PokemonDetailsDAO;
   private pokemonsFormat: PokemonsFormat = new PokemonsFormat();
 
   constructor (private services: Services) { }
 
   public async execute (pokemonNameOrId: string | number): Promise<PokemonEntity> {
     const data: PokemonDetailsDAO = await this.services.pokemons.searchByNameOrId(pokemonNameOrId);
-    this.pokemonDetails = data || {};
-    return this.pokemonsFormat.execute(this.pokemonDetails) as PokemonEntity;
+    return this.pokemonsFormat.execute(data || {}) as PokemonEntity;
   }
 }
