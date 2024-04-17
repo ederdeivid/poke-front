4c69fd881e031fbce1379786dcaa1be8b86c4566
Eder Deivid <eder.pereira@solucx.com.br>
2024-04-16 21:44:37 -0300

    chore: add gh-pages package and set vite config to works properly on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/poke-front/',
   plugins: [vue()],
   resolve: {
     alias: {
